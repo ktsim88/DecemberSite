5c6b51a344f569db3c04f1aad6b7c20b9ab0b40e
ktsim88 <526ktsim@frhsd.com>
2024-12-30 18:30:00 +0000

    fixed formating of code

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function genFirstName(firstName) {
   const firstLetter = firstName.charAt(0).toLowerCase();
   if (firstLetter === "a") {
     return "Applejack";
-  } else if (firstLetter === "c" || firstLetter === 'b') {
+  } else if (firstLetter === "c" || firstLetter === "b") {
     return "Child";
   } else if (firstLetter === "d") {
     return "Donut";
@@ -56,13 +56,13 @@ function genFirstName(firstName) {
 // generate middle name
 function getMiddleName(birthMonth, favColor) {
   // month with color
-  if (birthMonth === 'Jan' && favColor === 'red') {
+  if (birthMonth === "Jan" && favColor === "red") {
     return `Hellman`;
   } else if (birthMonth === "Jan" && favColor === "green") {
     return `Evergreen`;
-  } else if (birthMonth === "Jan" && favColor === 'blue') {
+  } else if (birthMonth === "Jan" && favColor === "blue") {
     return `Snowflake`;
-  }  else if (birthMonth === "Jan" && favColor === "yellow") {
+  } else if (birthMonth === "Jan" && favColor === "yellow") {
     return `Bellyman`;
   } else if (birthMonth === "Jan" && favColor === "white") {
     return `Snowman`;
@@ -79,8 +79,8 @@ function getMiddleName(birthMonth, favColor) {
     return `Cupidlike`;
   } else if (birthMonth === "Feb" && favColor === "black") {
     return `Emolike`;
-  } else if (birthMonth === 'Feb') { 
-    return `${favColor}like`
+  } else if (birthMonth === "Feb") {
+    return `${favColor}like`;
     // march
   } else if (birthMonth === "March" && favColor === "yellow") {
     return `Cattle`;
@@ -92,126 +92,126 @@ function getMiddleName(birthMonth, favColor) {
     return `Stock`;
   } else if (birthMonth === "March" && favColor === "red") {
     return `Angus`;
-  } else if (birthMonth === "March" && favColor === 'blue') {
+  } else if (birthMonth === "March" && favColor === "blue") {
     return `Longhorn`;
   } else if (birthMonth === "March") {
     return `${favColor}moo`;
     // april
-  } else if (birthMonth === "April" && favColor === 'blue') {
+  } else if (birthMonth === "April" && favColor === "blue") {
     return `Rainyside`;
-  } else if (birthMonth === "April" && favColor === 'purple') {
+  } else if (birthMonth === "April" && favColor === "purple") {
     return `Boiled`;
-  } else if (birthMonth === "April" && favColor === 'orange') {
+  } else if (birthMonth === "April" && favColor === "orange") {
     return `Yolkside`;
-  } else if (birthMonth === "April" && favColor === 'yellow') {
+  } else if (birthMonth === "April" && favColor === "yellow") {
     return `Sunnyside`;
   } else if (birthMonth === "April") {
     return `${favColor}side`;
     // may
-  } else if (birthMonth === "May" && favColor === 'orange') {
+  } else if (birthMonth === "May" && favColor === "orange") {
     return `Tangerine`;
-  } else if (birthMonth === "May" && favColor === 'pink') {
+  } else if (birthMonth === "May" && favColor === "pink") {
     return `Cherrypop`;
-  } else if (birthMonth === "May" && favColor === 'red') {
+  } else if (birthMonth === "May" && favColor === "red") {
     return `Bloodpop`;
-  } else if (birthMonth === "May" && favColor === 'blue') {
+  } else if (birthMonth === "May" && favColor === "blue") {
     return `Berrypop`;
-  } else if (birthMonth === "May" && favColor === 'yellow') {
+  } else if (birthMonth === "May" && favColor === "yellow") {
     return `Toxicpop`;
-  } else if (birthMonth === "May" && favColor === 'brown') {
+  } else if (birthMonth === "May" && favColor === "brown") {
     return `Poop`;
   } else if (birthMonth === "May") {
     return `${favColor}pop`;
     // june
-  } else if (birthMonth === "June" && favColor === 'purple') {
+  } else if (birthMonth === "June" && favColor === "purple") {
     return `Hexa`;
-  } else if (birthMonth === "June" && favColor === 'white') {
+  } else if (birthMonth === "June" && favColor === "white") {
     return `Triangular`;
-  } else if (birthMonth === "June" && favColor === 'pink') {
+  } else if (birthMonth === "June" && favColor === "pink") {
     return `Penta`;
-  } else if (birthMonth === "June" && favColor === 'black') {
+  } else if (birthMonth === "June" && favColor === "black") {
     return `Deca`;
-  } else if (birthMonth === "June" && favColor === 'orange') {
+  } else if (birthMonth === "June" && favColor === "orange") {
     return `Septa`;
   } else if (birthMonth === "June") {
     return `${favColor}ism`;
     // july (fyi zerose is a fandom name of a group. it's not a typo)
-  } else if (birthMonth === "July" && favColor === 'red') {
+  } else if (birthMonth === "July" && favColor === "red") {
     return `Rose`;
-  } else if (birthMonth === "July" && favColor === 'white') {
+  } else if (birthMonth === "July" && favColor === "white") {
     return `Zerose`;
-  } else if (birthMonth === "July" && favColor === 'yellow') {
+  } else if (birthMonth === "July" && favColor === "yellow") {
     return `Tulip`;
-  } else if (birthMonth === "July" && favColor === 'purple') {
+  } else if (birthMonth === "July" && favColor === "purple") {
     return `Lilac`;
   } else if (birthMonth === "July") {
     return `${favColor}bloom`;
     //august
-  } else if (birthMonth === "Aug" && favColor === 'red') {
+  } else if (birthMonth === "Aug" && favColor === "red") {
     return `Romeo`;
-  } else if (birthMonth === "Aug" && favColor === 'grey') {
+  } else if (birthMonth === "Aug" && favColor === "grey") {
     return `Sword`;
-  } else if (birthMonth === "Aug" && favColor === 'black') {
+  } else if (birthMonth === "Aug" && favColor === "black") {
     return `Romantic`;
-  } else if (birthMonth === "Aug" && favColor === 'pink') {
+  } else if (birthMonth === "Aug" && favColor === "pink") {
     return `Juliet`;
-  } else if (birthMonth === "Aug" && favColor === 'purple') {
+  } else if (birthMonth === "Aug" && favColor === "purple") {
     return `Julieta`;
-  } else if (birthMonth === "Aug" && favColor === 'orange') {
+  } else if (birthMonth === "Aug" && favColor === "orange") {
     return `Jest`;
   } else if (birthMonth === "Aug") {
     return `${favColor}lake`;
     //september
-  } else if (birthMonth === "Sept" && favColor === 'blue') {
+  } else if (birthMonth === "Sept" && favColor === "blue") {
     return `Starry`;
-  } else if (birthMonth === "Sept" && favColor === 'black') {
+  } else if (birthMonth === "Sept" && favColor === "black") {
     return `Darken`;
-  } else if (birthMonth === "Sept" && favColor === 'orange') {
+  } else if (birthMonth === "Sept" && favColor === "orange") {
     return `Toxin`;
-  } else if (birthMonth === "Sept" && favColor === 'purple') {
+  } else if (birthMonth === "Sept" && favColor === "purple") {
     return `Twinkle`;
-  } else if (birthMonth === "Sept" && favColor === 'pink') {
+  } else if (birthMonth === "Sept" && favColor === "pink") {
     return `Twilight`;
   } else if (birthMonth === "Sept") {
     return `${favColor}light`;
     //october
-  } else if (birthMonth === "Oct" && favColor === 'red') {
+  } else if (birthMonth === "Oct" && favColor === "red") {
     return `Candy`;
-  } else if (birthMonth === "Oct" && favColor === 'orange') {
+  } else if (birthMonth === "Oct" && favColor === "orange") {
     return `Pumpkin`;
-  } else if (birthMonth === "Oct" && favColor === 'yellow') {
+  } else if (birthMonth === "Oct" && favColor === "yellow") {
     return `Sunset`;
-  } else if (birthMonth === "Oct" && favColor === 'purple') {
+  } else if (birthMonth === "Oct" && favColor === "purple") {
     return `Witch`;
-  } else if (birthMonth === "Oct" && favColor === 'grey') {
+  } else if (birthMonth === "Oct" && favColor === "grey") {
     return `Ghost`;
-  } else if (birthMonth === "Oct" && favColor === 'black') {
+  } else if (birthMonth === "Oct" && favColor === "black") {
     return `Baddie`;
-  } else if (birthMonth === "Oct" && favColor === 'blue') {
+  } else if (birthMonth === "Oct" && favColor === "blue") {
     return `Nightly`;
   } else if (birthMonth === "Oct") {
     return `${favColor}ly`;
     //november
-  } else if (birthMonth === "Nov" && favColor === 'red') {
+  } else if (birthMonth === "Nov" && favColor === "red") {
     return `Turkey`;
-  } else if (birthMonth === "Nov" && favColor === 'brown') {
+  } else if (birthMonth === "Nov" && favColor === "brown") {
     return `Stuffing`;
-  } else if (birthMonth === "Nov" && favColor === 'orange') {
+  } else if (birthMonth === "Nov" && favColor === "orange") {
     return `Cranberry`;
-  } else if (birthMonth === "Nov" && favColor === 'green') {
+  } else if (birthMonth === "Nov" && favColor === "green") {
     return `Leaf`;
-  } else if (birthMonth === "Nov" && favColor === 'white') {
+  } else if (birthMonth === "Nov" && favColor === "white") {
     return `Powder`;
   } else if (birthMonth === "Nov") {
     return `${favColor}feast`;
     //december
   } else if (birthMonth === "Dec" && favColor === "white") {
     return `Snowfall`;
-  } else if (birthMonth === "Dec" && favColor === 'red') {
+  } else if (birthMonth === "Dec" && favColor === "red") {
     return `Festive`;
-  } else if (birthMonth === "Dec" && favColor === 'yellow') {
+  } else if (birthMonth === "Dec" && favColor === "yellow") {
     return `Melted`;
-  } else if (birthMonth === "Dec" && favColor === 'green') {
+  } else if (birthMonth === "Dec" && favColor === "green") {
     return `Wreath`;
   } else {
     return `${favColor}line`;
@@ -257,7 +257,7 @@ function genLastName(lastName) {
     return "Storm";
   } else if (lastLetter === "t") {
     return "Custard";
-  } else { 
+  } else {
     return "Simp";
   }
 }
@@ -265,21 +265,21 @@ function genLastName(lastName) {
 function genSuffix(favAnimal) {
   const endAnimal = favAnimal.charAt(favAnimal.length - 1).toLowerCase();
   switch (endAnimal) {
-    case 'a':
+    case "a":
       return `${favAnimal}ian`;
-    case 'e':
-      return `${favAnimal}yz`
-    case 'h':
-      return `${favAnimal}tailz`
-    case 'i':
-      return `${favAnimal}wan`
-    case 'o':
-      return `${favAnimal}'ed`
-    case 'u':
-      return `${favAnimal}uuu~`
+    case "e":
+      return `${favAnimal}yz`;
+    case "h":
+      return `${favAnimal}tailz`;
+    case "i":
+      return `${favAnimal}wan`;
+    case "o":
+      return `${favAnimal}'ed`;
+    case "u":
+      return `${favAnimal}uuu~`;
     default:
-      return `${favAnimal}fish`
-  } 
+      return `${favAnimal}fish`;
+  }
 }
 
 //master function to assemble full name
@@ -302,19 +302,16 @@ function getFullName() {
   const capitalizePrefix = capitalize(prefix);
   const capitalizeFirstName = capitalize(newFirstName);
   const capitalizeMiddleName = capitalize(middleName);
-    const capitalizeLastName = capitalize(newlastName);
-    const capitalizeSuffix = capitalize(suffix)
-    
-    
-// combine into final name
-const fullName = `${capitalizePrefix} ${capitalizeFirstName} ${capitalizeMiddleName} ${capitalizeLastName} ${capitalizeSuffix} Cookie`;
+  const capitalizeLastName = capitalize(newlastName);
+  const capitalizeSuffix = capitalize(suffix);
 
-document.getElementById("result").textContent = fullName;
+  // combine into final name
+  const fullName = `${capitalizePrefix} ${capitalizeFirstName} ${capitalizeMiddleName} ${capitalizeLastName} ${capitalizeSuffix} Cookie`;
+
+  document.getElementById("result").textContent = fullName;
 }
-
 
 // capitalzer function
 function capitalize(word) {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
-
